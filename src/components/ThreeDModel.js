329843d22cdbb262993940018d60e2866aec10d3
Niklas Daute <niklasdaute@icloud.com>
2024-08-10 00:30:20 +0200

    adjustments

--- src/components/ThreeDModel.js
+++ src/components/ThreeDModel.js
@@ -13,15 +13,15 @@ const ThreeDModel = ({ modelPath }) => {
   return (
     <Canvas
       style={{ height: '100vh', width: '100vw' }} // Canvas auf volle Höhe und Breite des Viewports setzen
-      camera={{ position: [0, 0, 5], fov: 75 }} // Kamera in Position setzen
+      camera={{ position: [2, 3, -6], fov: 75 }} // Kamera in Position setzen
     >
       <ambientLight intensity={0.8} />
       <pointLight position={[10, 10, 10]} intensity={0.6} />
       <pointLight position={[-10, -10, -10]} intensity={0.6} />
-      <OrbitControls enableZoom={false} />
+      <OrbitControls enableZoom={false} enablePan={false} />
       <Center>
         {/* Hier wird die Skalierung des Modells angepasst */}
-        <primitive object={scene} scale={[0.5, 0.5, 0.5]} />
+        <primitive object={scene} scale={[0.2, 0.2, 0.2]} />
       </Center>
     </Canvas>
   );
